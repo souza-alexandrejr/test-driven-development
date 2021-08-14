488a710cf946182c37ab3c238911f80b9e8904ac
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-08-14 11:02:47 -0300

    Adicao de novos testes e validacao com objetos

--- unit-tests/src/test/java/com/tdd/unit/tests/utils/MaiorEMenorTest.java
+++ unit-tests/src/test/java/com/tdd/unit/tests/utils/MaiorEMenorTest.java
@@ -9,27 +9,61 @@ import com.tdd.unit.tests.model.Produto;
 
 public class MaiorEMenorTest {
 	
+	/*
+	 * Cenarios a serem testados:
+	 * 	- Produtos com valores em ordem crescente
+	 * 	- Produtos com valores em ordem variada
+	 * 	- Um unico produto no carrinho
+	 */
+	
 	@Test
-	public void ordemDescrescente() {
-		
-		// Definicao do Cenario
+	public void apenasUmProduto() {
+		Produto produto = new Produto("Geladeira", 450.0);
 		
 		CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
-		
-		carrinhoDeCompras.adiciona(new Produto("Geladeira", 450.0));
-		carrinhoDeCompras.adiciona(new Produto("Liquificador", 250.0));
-		carrinhoDeCompras.adiciona(new Produto("Jogo de Pratos", 70.0));
-		
-		// Execucao de uma acao
-		
-		MaiorEMenor algoritmo =  new MaiorEMenor();
-		
+		carrinhoDeCompras.adiciona(produto);
+
+		MaiorEMenor algoritmo = new MaiorEMenor();
 		algoritmo.encontra(carrinhoDeCompras);
 		
-		// Validacao dos resultados
+		assertEquals(produto, algoritmo.getMenor());
+		assertEquals(produto, algoritmo.getMaior());
+	}
+	
+	@Test
+	public void ordemDescrescente() {
+		Produto produto1 = new Produto("Geladeira", 450.0);
+		Produto produto2 = new Produto("Liquificador", 250.0);
+		Produto produto3 = new Produto("Jogo de Pratos", 70.0);
 		
-		assertEquals("Jogo de Pratos", algoritmo.getMenor().getNome());
-		assertEquals("Geladeira", algoritmo.getMaior().getNome());
+		CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+		carrinhoDeCompras.adiciona(produto1);
+		carrinhoDeCompras.adiciona(produto2);
+		carrinhoDeCompras.adiciona(produto3);
+		
+		MaiorEMenor algoritmo =  new MaiorEMenor();
+		algoritmo.encontra(carrinhoDeCompras);
+		
+		assertEquals(produto3, algoritmo.getMenor());
+		assertEquals(produto1, algoritmo.getMaior());
+	}
+	
+	@Test
+	public void ordemVariada() {
+		Produto produto1 = new Produto("Liquificador", 250.0);
+		Produto produto2 = new Produto("Geladeira", 450.0);
+		Produto produto3 = new Produto("Jogo de Pratos", 70.0);
+		
+		CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+		carrinhoDeCompras.adiciona(produto1);
+		carrinhoDeCompras.adiciona(produto2);
+		carrinhoDeCompras.adiciona(produto3);
+		
+		MaiorEMenor algoritmo =  new MaiorEMenor();
+		algoritmo.encontra(carrinhoDeCompras);
+		
+		assertEquals(produto3, algoritmo.getMenor());
+		assertEquals(produto2, algoritmo.getMaior());
 	}
 
 }
