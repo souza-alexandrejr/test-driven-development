11906b59163d5d3925177900e1b54039664142bb
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-08-14 10:38:34 -0300

    Caso que falha + Cenario/Acao/Validacao

--- unit-tests/src/test/java/com/tdd/unit/tests/utils/MaiorEMenorTest.java
+++ unit-tests/src/test/java/com/tdd/unit/tests/utils/MaiorEMenorTest.java
@@ -6,15 +6,22 @@ import com.tdd.unit.tests.model.Produto;
 public class MaiorEMenorTest {
 	
 	public static void main(String[] args) {
+		
+		// Definicao do Cenario
+		
 		CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
 		
-		carrinhoDeCompras.adiciona(new Produto("Liquificador", 250.0));
 		carrinhoDeCompras.adiciona(new Produto("Geladeira", 450.0));
+		carrinhoDeCompras.adiciona(new Produto("Liquificador", 250.0));
 		carrinhoDeCompras.adiciona(new Produto("Jogo de Pratos", 70.0));
+		
+		// Execucao de uma acao
 		
 		MaiorEMenor algoritmo =  new MaiorEMenor();
 		
 		algoritmo.encontra(carrinhoDeCompras);
+		
+		// Validacao dos resultados
 		
 		System.out.println("O menor produto: " + algoritmo.getMenor().getNome());
 		System.out.println("O maior produto: " + algoritmo.getMaior().getNome());
